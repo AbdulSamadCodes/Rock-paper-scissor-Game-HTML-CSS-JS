622d4635ccfcb8c379d24e848a90b751d0ad9b69
Abdul Samad <samadmirza269@gmail.com>
2024-09-20 17:16:22 +0500

    Scoreboard remaining

--- src/render__contest.js
+++ src/render__contest.js
@@ -1,14 +1,14 @@
-import { GameOption,renderGameOption } from "./render_game_options.js";
+import { GameOption, renderGameOption } from "./render_game_options.js";
 
 const gameWindowWrapper = document.querySelector("[data-dame-window-wrapper]");
 const pentagonFigureWrapper = document.querySelector("[data-pentagon-figure-wrapper]");
 
 //function to handle the contest creation
 function handleContestCreation() {
-  pentagonFigureWrapper.addEventListener("click",(event) => {    
-    if(!(event.target.classList.contains("game-option") ||  event.target.parentElement.matches(".game-option"))) return;
+  pentagonFigureWrapper.addEventListener("click", (event) => {
+    if (!(event.target.classList.contains("game-option") || event.target.parentElement.matches(".game-option"))) return;
 
-    const userSelectedOption = event.target.classList.contains("game-option") ? event.target : event.target.parentElement ;
+    const userSelectedOption = event.target.classList.contains("game-option") ? event.target : event.target.parentElement;
 
     handleContest(userSelectedOption.dataset.option);
   });
@@ -21,6 +21,8 @@ const removeGameState = state => state.style.display = "none";
 
 //function to handle the contest
 function handleContest(userSelectedOption) {
+  let userSelectedObject, CPUSelectedObject;
+
   removeGameState(pentagonFigureWrapper);
 
   const contestOptionsContainer = document.createElement("div");
@@ -33,7 +35,7 @@ function handleContest(userSelectedOption) {
   const CPUOptionWrapper = document.createElement("div");
 
   [userOptionWrapper, CPUOptionWrapper].forEach((optionWrapper) => {
-    optionWrapper.classList.add("option-wrapper", "flex","justify-content-center","align-items-center");
+    optionWrapper.classList.add("option-wrapper", "flex", "justify-content-center", "align-items-center");
 
     const wrapperText = document.createElement("p");
     wrapperText.classList.add("option__wrapper-text", "uppercase");
@@ -45,7 +47,7 @@ function handleContest(userSelectedOption) {
   const userOptionContainer = document.createElement("span");
   const CPUOptionContainer = document.createElement("span");
 
-  [userOptionContainer, CPUOptionContainer].forEach((optionContainer,index) => {
+  [userOptionContainer, CPUOptionContainer].forEach((optionContainer, index) => {
     optionContainer.classList.add("option-container");
   });
 
@@ -56,29 +58,77 @@ function handleContest(userSelectedOption) {
   CPUOptionWrapper.appendChild(CPUOptionContainer);
 
   contestFragment.appendChild(contestOptionsContainer);
-  gameWindowWrapper.appendChild(contestFragment);    
+  gameWindowWrapper.appendChild(contestFragment);
 
-  //display user option
-  displayUserOption(userSelectedOption,userOptionContainer);
+  //render and  user option
+  userSelectedObject = renderUserOption(userSelectedOption, userOptionContainer);
 
-  //display CPU option
-  setTimeout(displayCPUOption,2000,CPUOptionContainer);
+  //render and get the CPU option
+  setTimeout(() => {
+    CPUSelectedObject = renderCPUOption(CPUOptionContainer);
+    const winnerOrDraw = validateContest(userSelectedObject, CPUSelectedObject);
+
+    displayResult(gameWindowWrapper, winnerOrDraw);
+  }, 2000);
 }
 
-//function to dislay the user selected option on contest window
-function displayUserOption(userOption,optionContainer){
-  const userSelectedOption = gameOptions.find((gameOption) => gameOption.name === userOption);
+//function to render the user selected option
+function renderUserOption(userOption, optionContainer) {
+  const userSelectedObject = gameOptions.find((gameOption) => gameOption.name === userOption);
 
-  renderGameOption(optionContainer,userSelectedOption.name,userSelectedOption.metaData.imgSrc);
+  renderGameOption(optionContainer, userSelectedObject.name, userSelectedObject.metaData.imgSrc);
+
+  return userSelectedObject;
 }
 
-//function to dislay the random CPU selected option on contest window
-function displayCPUOption(optionContainer) {
+//function to select and render the random CPU selected option
+function renderCPUOption(optionContainer) {
   const totalGameOptions = gameOptions.length;
 
   const CPUSelectedIndex = Math.floor(Math.random() * totalGameOptions);
-  const CPUSelectedOption = gameOptions[CPUSelectedIndex];
+  const CPUSelectedObject = gameOptions[CPUSelectedIndex];
 
-  renderGameOption(optionContainer,CPUSelectedOption.name,CPUSelectedOption.metaData.imgSrc);
+  renderGameOption(optionContainer, CPUSelectedObject.name, CPUSelectedObject.metaData.imgSrc);
+
+  return CPUSelectedObject;
+}
+
+//function to validate the contest results and display on UI
+function validateContest(userSelectedObject, CPUSelectedObject) {
+  let winnerOrDraw;
+  if (userSelectedObject.name === CPUSelectedObject.name) {
+    winnerOrDraw = "Draw";
+    return winnerOrDraw;
+  };
+
+  winnerOrDraw = userSelectedObject.beats.includes(CPUSelectedObject.name) ? "You win" : "You Lose";
+
+  return winnerOrDraw;
+}
+
+//function to display the result on UI
+function displayResult(parentElement, winnerOrDraw) {
+  const resultInfo = document.createElement("div");
+  resultInfo.classList.add("result-info", "grid", "margin-inline-auto");
+
+  const winnerInfo = document.createElement("span");
+  winnerInfo.classList.add("winner-info", "uppercase");
+  winnerInfo.textContent = winnerOrDraw;
+
+  const playAgainBtn = document.createElement("button");
+  playAgainBtn.classList.add("play-again-btn", "uppercase");
+  playAgainBtn.textContent = "Play Again";
+  playAgainBtn.addEventListener("click", createNewContest);
+
+  [winnerInfo, playAgainBtn].forEach(elements => resultInfo.appendChild(elements));
+
+  parentElement.appendChild(resultInfo);
+}
+
+//function to create the new contest
+function createNewContest() {
+  Array.from(gameWindowWrapper.children).filter((childElement) => childElement !== pentagonFigureWrapper).forEach(childElement => childElement.remove());
+
+  pentagonFigureWrapper.style.display = "block";
 }
 
